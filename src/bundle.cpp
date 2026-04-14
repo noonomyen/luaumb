4e0917534b7726b1df3ff47ebc35fb0afdaae687
Nomyen Arthit Saikanok <nomyen@noonomyen.com>
2026-04-14 19:08:27 +0700

    remove: scan remove EOL and dup newline, `find` and `erase` function is overhead when file is larger

--- src/bundle.cpp
+++ src/bundle.cpp
@@ -109,10 +109,5 @@ string LuauModuleBundle::build() {
 
     out += "_LUAUMB_" + nonstd_base64::encode(this->main_path.relative.string()) + "()";
 
-    size_t pos = 0;
-    while ((pos = out.find("\r\n", pos)) != string::npos) out.erase(pos, 1);
-    pos = 0;
-    while ((pos = out.find("\n\n", pos)) != string::npos) out.erase(pos, 1);
-
     return out;
 }
